209c33fc322cf37a55a12b80ae98f23339a5870e
hjenie <hjenie06@gmail.com>
2017-10-11 15:59:15 +0900

    fix flickering

--- heatmap.js
+++ heatmap.js
@@ -31,7 +31,7 @@ $(function() {
         "SoundWall3" : {x : 0.8 * height, y: 0.93 *height},
     };
 
-    MotionMap = {
+    MotionLocationMap = {
         "Mtest1": { x: 0.35 * height, y: 0.85*height },
         "Htest2": { x: 0.65*height, y: 0.85*height},
         "Mtest3": { x: 0.9*height, y: 0.85*height},
@@ -43,7 +43,7 @@ $(function() {
         "Mtest9": { x: 0.2 * height, y: 0.65*height },
     };
 
-    AmbientMap = {
+    AmbientLocationMap = {
         "sensor0_Temperature" : {x : height , y : 0.5 * height},
         "sensor1_Temperature" : {x :0.35 * height , y : 0.5 * height},
         "sensor0_Humidity" : {x : height, y : 0.5 * height},
@@ -56,26 +56,26 @@ $(function() {
                     map : SoundLocationMap,
                     valueType : "float",
                     min : 0,
-                    max : 100,     },
+                    max : 100,
+                    datapoints : []  },
         "Motion" : {url : "http://143.248.56.213:8104/context",
-                    map : MotionMap,
+                    map : MotionLocationMap,
                     valueType : "boolean",
                     min : 0,
-                    max : 100,       },
+                    max : 100,
+                    datapoints : []  },
         "Ambient" : {url : "http://143.248.56.213:8088/context",
-                    map : AmbientMap,
+                    map : AmbientLocationMap,
                     valueType : "float",
                     min : 0,
-                    max : 100,      },
+                    max : 100,
+                    datapoints : []  },
     };
 
     /* get json data and draw heatmap. */
-    datapoints = [];
     refreshData = function() {
-        datapoints = [];
-        height = document.getElementById("heatmapContainer").clientHeight;
         for (var sensor in Sensors){
-            datapoints = datapoints.concat(loadData(sensor, datapoints));
+            loadData(sensor);
         }
     };
     setInterval(refreshData, 1000);
@@ -83,45 +83,48 @@ $(function() {
     /* Get points for specific sensor type.
     *  SENSOR is key of Sensors and
     *  DATAPOINTS is array of heatmap points. */
-    function loadData( sensor , datapoints){
+    function loadData( sensor){
         var info = Sensors[sensor];
         var sensormap = info.map;
-        $.ajax({
+        var datapoints = [];
+        var getData = $.ajax({
             url: info.url,
             type: 'GET',
+            ifModified : true,
+            asynch : false,
             success: function(data) {
                 for (var i = data.length - 1; i >= 0; i--) {
-
                     var loc = sensormap[data[i].name];
                     if (loc == undefined) continue;
                     else if(info.valueType == "float") {
                         datapoints.push({ x: loc.x, y: loc.y, value: data[i].value * 100/ info.max });
-
                     } else if (info.valueType == "boolean") {
                         if (data[i].value == false) {
                             datapoints.push({x: loc.x, y: loc.y, value: 0});
                         }
                         else {
-                            datapoints.push({x: loc.x, y: loc.y, value: 1000});
+                            datapoints.push({x: loc.x, y: loc.y, value: 100});
                         }
                     }
                 }
-                drawHeatMap(datapoints);
+                drawHeatMap(sensor, datapoints);
+            },});
 
-            },
-            error: function(xhr, status, error) {
-                //alert("error");
-            }
-        });
-        return datapoints;
+
     }
 
     /* set points of heatmap. */
-    function drawHeatMap(datapoints){
+    function drawHeatMap(sensor, datapoints){
+        Sensors[sensor].datapoints = datapoints;
+        var points =  [];
+        for (var sensor in Sensors){
+            var info = Sensors[sensor];
+            points = points.concat(info.datapoints);
+        }
         heatmapInstance.setData({
             max : 100,
             min : 0,
-            data : datapoints
+            data : points
         });
     }
 
